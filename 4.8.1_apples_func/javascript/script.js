e32e4f2511fe00df76b2af78f9d37d125a8a400a
Shymee <48209491+Shymee@users.noreply.github.com>
2019-03-20 18:15:16 +0300

    4.8.1

--- 4.8.1_apples_func/javascript/script.js
+++ 4.8.1_apples_func/javascript/script.js
@@ -8,15 +8,14 @@ let AppleCost = prompt('Skolko stoit odno yabloko?')
 AppleCost = parseFloat(AppleCost)
 let BreadCost = prompt('Skolko stoit odin baton hleba?')
 BreadCost = parseFloat(BreadCost)
-let result = sumofMoney >= sumofApple*AppleCost+sumofBread*BreadCost
 
-function haveEnough(result) {
-	if (result > 0){
+function haveEnough(sumofMoney, sumofApple, sumofBread, AppleCost, BreadCost) {
+	if (sumofMoney > sumofApple * AppleCost + sumofBread * BreadCost){
 		return "Вам хватает денег на покупки";
 	} else {
 		return "Вам не хватает денег на покупки";
 	}
 }
 
-document.body.innerHTML = haveEnough(result);
+document.body.innerHTML = haveEnough(sumofMoney, sumofApple, sumofBread, AppleCost, BreadCost);
 
